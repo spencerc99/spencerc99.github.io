34126eb76a2dfbcc480cdc1252a37684e819647c
Spencer Chang <spencerc99@gmail.com>
2021-10-03 02:40:39 -0700

    testing

---
+++ assets/js/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+function App() {
+  return <>Hello React!</>;
+}
+
+ReactDOM.render(<App />, document.getElementById("react"));
