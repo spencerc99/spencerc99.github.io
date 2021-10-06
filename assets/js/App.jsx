01d3db6703d55c1c311a240885829525068b15bf
Spencer Chang <spencerc99@gmail.com>
2021-10-05 18:01:45 -0700

    support react

--- assets/js/App.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-function App() {
-  return <>Hello React!</>;
-}
-
-ReactDOM.render(<App />, document.getElementById("react"));
