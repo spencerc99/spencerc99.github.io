7e947957bf44dd99531cb82b0d1a53a1248a50cc
Spencer Chang <spencerc99@gmail.com>
2021-12-30 02:17:25 -0800

    one last try

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,7 @@
 #!/bin/sh
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Pull any other remote changes
-# if [ -d "public" ] 
-# then
-cd public
-git pull
-cd ..	
-# fi
-
-# Build the project.
-hugo --minify || exit
-
-# Go To Public folder
-cd public || exit
-
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
+./scripts/build-and-commit-changes.sh
 
 # Push source and build repos.
-git push origin master
+cd public && git push origin master
