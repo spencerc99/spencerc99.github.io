883ed970bb3e68bf881472345539823ee3a27ff6
Spencer Chang <spencerc99@gmail.com>
2021-12-31 01:59:38 -0800

    update deploy script to find linked articles

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+python ./scripts/find_linked_articles.py
 ./scripts/build-and-commit-changes.sh
 
 # Push source and build repos.
